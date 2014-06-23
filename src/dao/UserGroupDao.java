b70b485e3cc233e506b17a8feceaa0e6c65d8581
GiveMePeanutz <claudin.adrien@gmail.com>
2014-06-23 16:47:27 +0800

    kewa

--- src/dao/UserGroupDao.java
+++ src/dao/UserGroupDao.java
@@ -1,5 +1,22 @@
 package dao;
 
+import java.util.List;
+
+import beans.Group;
+import beans.User;
+
 public interface UserGroupDao {
 
+	void create( int groupID, String username ) throws DAOException;
+	
+	void delete (int groupID, String username ) throws DAOException;
+	
+	void deleteBygroupID (int groupID) throws DAOException;
+	
+	void deleteByPrivID (String username ) throws DAOException;
+	
+	List<User> listGroupUsers( int groupID ) throws DAOException;
+	
+	List<Group> listUserGroups( String username ) throws DAOException;
+	
 }
