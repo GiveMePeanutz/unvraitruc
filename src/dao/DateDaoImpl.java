62c1fd1c6a368b1fa7abac873dc9c4d4688be311
Florian <Florian.guihard@gmail.com>
2014-07-11 17:09:04 +0800

    sdfsfd

--- src/dao/DateDaoImpl.java
+++ src/dao/DateDaoImpl.java
@@ -2,10 +2,13 @@ package dao;
 
 import org.joda.time.DateTime;
 
+import beans.Date;
+
 public class DateDaoImpl implements DateDao {
 
-    public void createDate( DateTime dateTime ) throws DAOException
+    public Date createDate( DateTime dateTime ) throws DAOException
     {
+        return null;
 
     }
 
