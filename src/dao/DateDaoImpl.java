467a0ae50ed4cc2fa752221e85c0161e213a50d6
Florian <Florian.guihard@gmail.com>
2014-07-11 17:07:16 +0800

    qdfdsf

---
+++ src/dao/DateDaoImpl.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.joda.time.DateTime;
+
+public class DateDaoImpl implements DateDao {
+
+    public void createDate( DateTime dateTime ) throws DAOException
+    {
+
+    }
+
+}
