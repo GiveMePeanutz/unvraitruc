e84b552f68a9365e21653337a2d9263e030f7461
GiveMePeanutz <claudin.adrien@gmail.com>
2014-07-11 16:46:37 +0800

    aefaerf

--- src/beans/date.java
+++ src/beans/date.java
@@ -1,6 +1,14 @@
 package beans;
 
-public class date {
+public class Date {
+	private int dateID;
+	private int minute;
 	private int hour;
-	
+	private int day;
+	private String dayName;
+	private int week;
+	private int month;
+	private String monthName;
+	private int quarter;
+	private int year;
 }
