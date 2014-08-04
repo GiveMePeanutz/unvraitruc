1495a5ecff66aa9a5b19292c74aacfd6575bff69
Florian <Florian.guihard@gmail.com>
2014-08-04 14:21:27 +0800

    com menu

--- src/staticData/Menu.java
+++ src/staticData/Menu.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 
 public final class Menu {
 
+    // hard coded privileges' menu
     private static String[][][] menus =
                                       {
                                       {
@@ -52,6 +53,7 @@ public final class Menu {
         return menus;
     }
 
+    // returns the name of the menu corresponding to the path in parameter.
     public static String getMenuName( int path ) throws ArrayIndexOutOfBoundsException {
         try {
             return menus[path / 10000][( path - ( path / 10000 ) * 10000 ) / 100][( path - ( path / 100 ) * 100 )];
@@ -61,6 +63,7 @@ public final class Menu {
         }
     }
 
+    // returns a map of all menu names, key is the path, value is the name
     public static LinkedHashMap<Integer, String> list()
     {
         LinkedHashMap<Integer, String> mapMenus = new LinkedHashMap<Integer, String>();
@@ -80,6 +83,7 @@ public final class Menu {
         return mapMenus;
     }
 
+    // returns the path of the menu corresponding to the name in parameter.
     public static int getPath( int firstmenu, int submenu, int subsubmenu ) {
 
         return ( firstmenu * 10000 + submenu * 100 + subsubmenu );
